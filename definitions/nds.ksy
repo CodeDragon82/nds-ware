4b88d3a005405d01daf5e94c39e09f523ba24eb5
CodeDragon82 <michaelrichar280@outlook.com>
2024-12-07 14:50:56 +0000

    Added kaitai definition and parser for nds files.

---
+++ definitions/nds.ksy
@@ -0,0 +1,312 @@
+meta:
+  id: nds
+  endian: le
+  encoding: ascii
+  file-extension: nds
+
+enums:
+  unit_code_enum:
+    0: nds
+    2: nds_dsi
+    3: dsi
+
+types:
+  header:
+    seq:
+      - id: game_title
+        type: str
+        size: 12
+      - id: game_code
+        size: 4
+      - id: maker_code
+        type: str
+        size: 2
+      - id: unit_code
+        type: u1
+        enum: unit_code_enum
+      - id: encryption_seed
+        type: u1
+      - id: device_capcity
+        type: u1
+      - id: reserved
+        size: 7
+      - id: game_revision
+        size: 2
+      - id: rom_version
+        type: u1
+      - id: internal_flags
+        type: u1
+      - id: arm9
+        type: code_section_info
+      - id: arm7
+        type: code_section_info
+      - id: fnt_info
+        type: section_info
+      - id: fat_info
+        type: section_info
+      - id: arm9_overlay
+        type: section_info
+      - id: arm7_overlay
+        type: section_info
+      - id: normal_card_control_register_settings
+        size: 4
+      - id: secure_card_control_register_settings
+        size: 4
+      - id: icon_banner_offset
+        type: u4
+      - id: secure_area_crc
+        type: u2
+      - id: secure_transfer_timeout
+        type: u2
+      - id: arm9_autoload
+        type: u4
+      - id: arm7_autoload
+        type: u4
+      - id: secure_disable
+        type: u8
+      - id: ntr_region_rom_size
+        type: u4
+      - id: header_size
+        type: u4
+      - id: reserved2
+        size: 56
+      - id: nintendo_logo
+        size: 156
+      - id: nintendo_logo_crc
+        type: u2
+      - id: header_crc
+        type: u2
+      - id: debugger_reserved
+        size: 32
+  
+  extended_header:
+    seq:
+      - id: global_mbk1_5_settings
+        size: 20
+      - id: local_mbk6_8_settings_for_arm9
+        size: 12
+      - id: local_mbk6_8_settings_for_arm7
+        size: 12
+      - id: global_mdk9_setting
+        size: 4
+      - id: region_flags
+        size: 4
+      - id: access_control
+        size: 4
+      - id: arm7_scrg_ext_mask
+        size: 4
+      - id: reserved
+        size: 4
+      - id: arm9i
+        type: code_section_info
+      - id: arm7i
+        type: code_section_info
+      - id: digest_ntr_region
+        type: section_info
+      - id: digest_twl_region
+        type: section_info
+      - id: digest_sector_hashtable
+        type: section_info
+      - id: digest_block_hashtable
+        type: section_info
+      - id: digest_section_size
+        type: u4
+      - id: digest_block_sectorcount
+        type: u4
+      - id: icon_banner_size
+        type: u4
+      - id: un1
+        type: u4
+      - id: ntr_twl_region_rom_size
+        type: u4
+      - id: un2
+        size: 12
+      - id: modcrypt_area_1
+        type: section_info
+      - id: modcrypt_area_2
+        type: section_info
+      - id: tital_id
+        type: u8
+
+  code_section_info:
+    -webide-representation: '{offset} {size:dec}B'
+    seq:
+      - id: offset
+        type: u4
+      - id: entry_address
+        type: u4
+      - id: load_address
+        type: u4
+      - id: size
+        type: u4
+  
+  section_info:
+    seq:
+      - id: offset
+        type: u4
+      - id: size
+        type: u4
+        
+  code_section:
+    -webide-representation: '{info}'
+    params:
+      - id: info
+        type: code_section_info
+    instances:
+      data:
+        pos: info.offset
+        size: info.size
+  
+  ### File Name Table ###      
+  
+  file_name_table:
+    seq:
+      - id: magic
+        type: u4
+      - id: section_size
+        type: u2
+      - id: directory_count
+        type: u2
+      - id: directory_table
+        type: directory_entry(_index + 1)
+        repeat: expr
+        repeat-expr: directory_count - 1
+      - id: directories
+        type: directory
+        repeat: expr
+        repeat-expr: directory_count
+        
+  directory_entry:
+    -webide-representation: 'id:{id} parent_id:{parent_directory}'
+    params:
+      - id: id
+        type: u4
+    seq:
+      - id: directory_offset
+        type: u4
+      - id: first_file_position
+        type: u2
+      - id: parent_directory
+        type: u2
+        
+  directory:
+    -webide-representation: '{content}'
+    seq:
+      - id: content
+        type: directory_content
+        terminator: 0
+  
+  directory_content:
+    -webide-representation: '{files}'
+    seq:
+      - id: files
+        type: file_entry
+        repeat: eos
+        
+  file_entry: 
+    -webide-representation: '{name}:{directory_id}'
+    seq:
+      - id: flag
+        type: file_flag
+      - id: name
+        type: str
+        size: flag.name_length
+      - id: directory_id
+        type: u2
+        if: flag.is_directory
+        
+  file_flag:
+    seq:
+      - id: is_directory
+        type: b1
+      - id: name_length
+        type: b7
+  
+  ##### #####
+  
+  ### File Allocation Table ###
+        
+  fat_entry:
+    -webide-representation: '{start_offset}-{end_offset}'
+    seq:
+      - id: start_offset
+        type: u4
+      - id: end_offset
+        type: u4
+  
+  ##### #####
+  
+  string:
+    -webide-representation: '{value}'
+    seq:
+      - id: length
+        type: u1
+      - id: value
+        size: length
+        type: str
+        
+  overlay_table:
+    seq:
+      - id: entries
+        type: overlay_entry
+        repeat: expr
+        repeat-expr: 236
+        
+  overlay_entry:
+    -webide-representation: 'index: {index:dec}'
+    seq:
+      - id: index
+        type: u4
+      - id: base_address
+        type: u4
+      - id: length
+        type: u4
+      - id: bss_size
+        type: u4
+      - id: start_address
+        type: u4
+      - id: end_address
+        type: u4
+      - id: file_id
+        type: u4
+      - id: reserved
+        type: u4
+  
+  file:
+    -webide-representation: '{info} {data}'
+    params:
+      - id: info
+        type: fat_entry
+    instances:
+      data:
+        pos: info.start_offset
+        size: info.end_offset - info.start_offset
+
+seq:
+  - id: header
+    type: header
+  - id: extended_header
+    type: extended_header
+  - id: files
+    type: file(file_allocation_table[_index])
+    repeat: expr
+    repeat-expr: file_allocation_table.size
+  - id: arm9
+    type: code_section(header.arm9)
+  - id: arm7
+    type: code_section(header.arm7)
+  - id: arm9i
+    type: code_section(extended_header.arm9i)
+  - id: arm7i
+    type: code_section(extended_header.arm7i)
+  
+instances:
+  file_name_table:
+    pos: header.fnt_info.offset
+    size: header.fnt_info.size
+    type: file_name_table
+  file_allocation_table:
+    pos: header.fat_info.offset
+    type: fat_entry
+    repeat: expr
+    repeat-expr: header.fat_info.size / 8
---
+++ parsers/nds.py
@@ -0,0 +1,355 @@
+# This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+import kaitaistruct
+from kaitaistruct import KaitaiStruct, KaitaiStream, BytesIO
+from enum import Enum
+
+
+if getattr(kaitaistruct, 'API_VERSION', (0, 9)) < (0, 9):
+    raise Exception("Incompatible Kaitai Struct Python API: 0.9 or later is required, but you have %s" % (kaitaistruct.__version__))
+
+class Nds(KaitaiStruct):
+
+    class UnitCodeEnum(Enum):
+        nds = 0
+        nds_dsi = 2
+        dsi = 3
+    def __init__(self, _io, _parent=None, _root=None):
+        self._io = _io
+        self._parent = _parent
+        self._root = _root if _root else self
+        self._read()
+
+    def _read(self):
+        self.header = Nds.Header(self._io, self, self._root)
+        self.extended_header = Nds.ExtendedHeader(self._io, self, self._root)
+        self.files = []
+        for i in range(len(self.file_allocation_table)):
+            self.files.append(Nds.File(self.file_allocation_table[i], self._io, self, self._root))
+
+        self.arm9 = Nds.CodeSection(self.header.arm9, self._io, self, self._root)
+        self.arm7 = Nds.CodeSection(self.header.arm7, self._io, self, self._root)
+        self.arm9i = Nds.CodeSection(self.extended_header.arm9i, self._io, self, self._root)
+        self.arm7i = Nds.CodeSection(self.extended_header.arm7i, self._io, self, self._root)
+
+    class CodeSection(KaitaiStruct):
+        def __init__(self, info, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self.info = info
+            self._read()
+
+        def _read(self):
+            pass
+
+        @property
+        def data(self):
+            if hasattr(self, '_m_data'):
+                return self._m_data
+
+            _pos = self._io.pos()
+            self._io.seek(self.info.offset)
+            self._m_data = self._io.read_bytes(self.info.size)
+            self._io.seek(_pos)
+            return getattr(self, '_m_data', None)
+
+
+    class FileNameTable(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.magic = self._io.read_u4le()
+            self.section_size = self._io.read_u2le()
+            self.directory_count = self._io.read_u2le()
+            self.directory_table = []
+            for i in range((self.directory_count - 1)):
+                self.directory_table.append(Nds.DirectoryEntry((i + 1), self._io, self, self._root))
+
+            self.directories = []
+            for i in range(self.directory_count):
+                self.directories.append(Nds.Directory(self._io, self, self._root))
+
+
+
+    class OverlayEntry(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.index = self._io.read_u4le()
+            self.base_address = self._io.read_u4le()
+            self.length = self._io.read_u4le()
+            self.bss_size = self._io.read_u4le()
+            self.start_address = self._io.read_u4le()
+            self.end_address = self._io.read_u4le()
+            self.file_id = self._io.read_u4le()
+            self.reserved = self._io.read_u4le()
+
+
+    class DirectoryEntry(KaitaiStruct):
+        def __init__(self, id, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self.id = id
+            self._read()
+
+        def _read(self):
+            self.directory_offset = self._io.read_u4le()
+            self.first_file_position = self._io.read_u2le()
+            self.parent_directory = self._io.read_u2le()
+
+
+    class String(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.length = self._io.read_u1()
+            self.value = (self._io.read_bytes(self.length)).decode(u"ascii")
+
+
+    class Files(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.files = []
+            i = 0
+            while not self._io.is_eof():
+                self.files.append(Nds.FileEntry(self._io, self, self._root))
+                i += 1
+
+
+
+    class SectionInfo(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.offset = self._io.read_u4le()
+            self.size = self._io.read_u4le()
+
+
+    class FatEntry(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.start_offset = self._io.read_u4le()
+            self.end_offset = self._io.read_u4le()
+
+
+    class OverlayTable(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.entries = []
+            for i in range(236):
+                self.entries.append(Nds.OverlayEntry(self._io, self, self._root))
+
+
+
+    class ExtendedHeader(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.global_mbk1_5_settings = self._io.read_bytes(20)
+            self.local_mbk6_8_settings_for_arm9 = self._io.read_bytes(12)
+            self.local_mbk6_8_settings_for_arm7 = self._io.read_bytes(12)
+            self.global_mdk9_setting = self._io.read_bytes(4)
+            self.region_flags = self._io.read_bytes(4)
+            self.access_control = self._io.read_bytes(4)
+            self.arm7_scrg_ext_mask = self._io.read_bytes(4)
+            self.reserved = self._io.read_bytes(4)
+            self.arm9i = Nds.CodeSectionInfo(self._io, self, self._root)
+            self.arm7i = Nds.CodeSectionInfo(self._io, self, self._root)
+            self.digest_ntr_region = Nds.SectionInfo(self._io, self, self._root)
+            self.digest_twl_region = Nds.SectionInfo(self._io, self, self._root)
+            self.digest_sector_hashtable = Nds.SectionInfo(self._io, self, self._root)
+            self.digest_block_hashtable = Nds.SectionInfo(self._io, self, self._root)
+            self.digest_section_size = self._io.read_u4le()
+            self.digest_block_sectorcount = self._io.read_u4le()
+            self.icon_banner_size = self._io.read_u4le()
+            self.un1 = self._io.read_u4le()
+            self.ntr_twl_region_rom_size = self._io.read_u4le()
+            self.un2 = self._io.read_bytes(12)
+            self.modcrypt_area_1 = Nds.SectionInfo(self._io, self, self._root)
+            self.modcrypt_area_2 = Nds.SectionInfo(self._io, self, self._root)
+            self.tital_id = self._io.read_u8le()
+
+
+    class Header(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.game_title = (self._io.read_bytes(12)).decode(u"ascii")
+            self.game_code = self._io.read_bytes(4)
+            self.maker_code = (self._io.read_bytes(2)).decode(u"ascii")
+            self.unit_code = KaitaiStream.resolve_enum(Nds.UnitCodeEnum, self._io.read_u1())
+            self.encryption_seed = self._io.read_u1()
+            self.device_capcity = self._io.read_u1()
+            self.reserved = self._io.read_bytes(7)
+            self.game_revision = self._io.read_bytes(2)
+            self.rom_version = self._io.read_u1()
+            self.internal_flags = self._io.read_u1()
+            self.arm9 = Nds.CodeSectionInfo(self._io, self, self._root)
+            self.arm7 = Nds.CodeSectionInfo(self._io, self, self._root)
+            self.fnt_info = Nds.SectionInfo(self._io, self, self._root)
+            self.fat_info = Nds.SectionInfo(self._io, self, self._root)
+            self.arm9_overlay = Nds.SectionInfo(self._io, self, self._root)
+            self.arm7_overlay = Nds.SectionInfo(self._io, self, self._root)
+            self.normal_card_control_register_settings = self._io.read_bytes(4)
+            self.secure_card_control_register_settings = self._io.read_bytes(4)
+            self.icon_banner_offset = self._io.read_u4le()
+            self.secure_area_crc = self._io.read_u2le()
+            self.secure_transfer_timeout = self._io.read_u2le()
+            self.arm9_autoload = self._io.read_u4le()
+            self.arm7_autoload = self._io.read_u4le()
+            self.secure_disable = self._io.read_u8le()
+            self.ntr_region_rom_size = self._io.read_u4le()
+            self.header_size = self._io.read_u4le()
+            self.reserved2 = self._io.read_bytes(56)
+            self.nintendo_logo = self._io.read_bytes(156)
+            self.nintendo_logo_crc = self._io.read_u2le()
+            self.header_crc = self._io.read_u2le()
+            self.debugger_reserved = self._io.read_bytes(32)
+
+
+    class File(KaitaiStruct):
+        def __init__(self, info, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self.info = info
+            self._read()
+
+        def _read(self):
+            pass
+
+        @property
+        def data(self):
+            if hasattr(self, '_m_data'):
+                return self._m_data
+
+            _pos = self._io.pos()
+            self._io.seek(self.info.start_offset)
+            self._m_data = self._io.read_bytes((self.info.end_offset - self.info.start_offset))
+            self._io.seek(_pos)
+            return getattr(self, '_m_data', None)
+
+
+    class FileEntry(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.flag = Nds.FileFlag(self._io, self, self._root)
+            self.name = (self._io.read_bytes(self.flag.name_length)).decode(u"ascii")
+            if self.flag.is_directory:
+                self.directory_id = self._io.read_u2le()
+
+
+
+    class FileFlag(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.is_directory = self._io.read_bits_int_be(1) != 0
+            self.name_length = self._io.read_bits_int_be(7)
+
+
+    class CodeSectionInfo(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.offset = self._io.read_u4le()
+            self.entry_address = self._io.read_u4le()
+            self.load_address = self._io.read_u4le()
+            self.size = self._io.read_u4le()
+
+
+    class Directory(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self._raw_content = self._io.read_bytes_term(0, False, True, True)
+            _io__raw_content = KaitaiStream(BytesIO(self._raw_content))
+            self.content = Nds.Files(_io__raw_content, self, self._root)
+
+
+    @property
+    def file_name_table(self):
+        if hasattr(self, '_m_file_name_table'):
+            return self._m_file_name_table
+
+        _pos = self._io.pos()
+        self._io.seek(self.header.fnt_info.offset)
+        self._raw__m_file_name_table = self._io.read_bytes(self.header.fnt_info.size)
+        _io__raw__m_file_name_table = KaitaiStream(BytesIO(self._raw__m_file_name_table))
+        self._m_file_name_table = Nds.FileNameTable(_io__raw__m_file_name_table, self, self._root)
+        self._io.seek(_pos)
+        return getattr(self, '_m_file_name_table', None)
+
+    @property
+    def file_allocation_table(self):
+        if hasattr(self, '_m_file_allocation_table'):
+            return self._m_file_allocation_table
+
+        _pos = self._io.pos()
+        self._io.seek(self.header.fat_info.offset)
+        self._m_file_allocation_table = []
+        for i in range(self.header.fat_info.size // 8):
+            self._m_file_allocation_table.append(Nds.FatEntry(self._io, self, self._root))
+
+        self._io.seek(_pos)
+        return getattr(self, '_m_file_allocation_table', None)
+
+
