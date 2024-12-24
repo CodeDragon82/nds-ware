715e6330719e49e1c88880403d7e92856b1d2f2a
CodeDragon82 <michaelrichar280@outlook.com>
2024-12-24 12:46:27 +0000

    Added a 'list' command to the extract_narc tool which displays files and directories in a Nintendo Archive without extracting it.

--- extract_narc.py
+++ extract_narc.py
@@ -21,6 +21,70 @@ def count(narc_file: str) -> None:
     print(f"{file_count} files")
 
 
+def list_directory(
+    fnt: Narc.Btnf,
+    directory: Narc.Directory,
+    depth: int,
+    max_depth: int,
+    show_option: str,
+) -> None:
+    for file in reversed(directory.content.files):
+        if show_option in ["directories", "all"]:
+            indent = depth * "  " + "-"
+        else:
+            indent = "-"
+
+        if file.flag.is_directory:
+            if show_option in ["directories", "all"]:
+                print(f"{indent} {file.name} (DIR)")
+
+            next_directory_index = file.directory_id & 0xFF
+            next_directory = fnt.directories[next_directory_index]
+
+            if depth < max_depth or max_depth == -1:
+                list_directory(
+                    fnt, next_directory, depth + 1, max_depth, show_option
+                )
+        elif show_option in ["files", "all"]:
+            print(f"{indent} {file.name}")
+
+
+@cli.command("list", help="Show files and folders within a Nintendo Archive.")
+@click.argument("narc_file")
+@click.option(
+    "-d",
+    "--max-depth",
+    type=int,
+    default=-1,
+    help="Displays files and folders up to N levels deep.",
+)
+@click.option(
+    "--directories",
+    "show_option",
+    flag_value="directories",
+    help="Only show directory structure.",
+)
+@click.option(
+    "--files",
+    "show_option",
+    flag_value="files",
+    help="Only show files; ignore directory structure.",
+)
+@click.option(
+    "--all",
+    "show_option",
+    flag_value="all",
+    default=True,
+    help="Show files and directory structure.",
+)
+def list_archive(narc_file: str, max_depth: int, show_option: str) -> None:
+    narc = Narc.from_file(narc_file)
+
+    fnt = narc.file_name_table.data
+
+    list_directory(fnt, fnt.directories[0], 0, max_depth, show_option)
+
+
 def extract_directory(
     files: list[Narc.File],
     fnt: Narc.Btnf,
