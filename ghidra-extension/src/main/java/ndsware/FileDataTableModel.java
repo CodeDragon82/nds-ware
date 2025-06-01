7ccdb8752d451e2a589867057d72b32fe39494e7
CodeDragon82 <michaelrichar280@outlook.com>
2025-06-01 11:15:49 +0100

    Create a new table model that can be used with Ghidra's 'GFilterTable' to show file data

---
+++ ghidra-extension/src/main/java/ndsware/FileDataTableModel.java
@@ -0,0 +1,79 @@
+package ndsware;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import docking.widgets.table.RowObjectTableModel;
+
+public class FileDataTableModel extends AbstractTableModel implements RowObjectTableModel<FileDataRow> {
+
+    private final String[] columnNames = { "Address", "Hex", "ASCII" };
+    private final List<FileDataRow> rows = new ArrayList<>();
+
+    public void addRow(FileDataRow newRow) {
+        rows.add(newRow);
+    }
+
+    public void clear() {
+        rows.clear();
+    }
+
+    @Override
+    public int getRowCount() {
+        return rows.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return columnNames[columnIndex];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        FileDataRow row = rows.get(rowIndex);
+        return switch (columnIndex) {
+            case 0 -> row.getAddress();
+            case 1 -> row.getHex();
+            case 2 -> row.getAscii();
+            default -> null;
+        };
+    }
+
+    @Override
+    public Object getColumnValueForRow(FileDataRow row, int columnIndex) {
+        return switch (columnIndex) {
+            case 0 -> row.getAddress();
+            case 1 -> row.getHex();
+            case 2 -> row.getAscii();
+            default -> null;
+        };
+    }
+
+    @Override
+    public List<FileDataRow> getModelData() {
+        return rows;
+    }
+
+    @Override
+    public String getName() {
+        return "test";
+    }
+
+    @Override
+    public int getRowIndex(FileDataRow row) {
+        return rows.indexOf(row);
+    }
+
+    @Override
+    public FileDataRow getRowObject(int rowIndex) {
+        return rows.get(rowIndex);
+    }
+
+}
