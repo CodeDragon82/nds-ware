1671239f81713169247e8875aa03be028f915c8d
CodeDragon82 <65504498+CodeDragon82@users.noreply.github.com>
2025-06-10 11:23:14 +0100

    Add graphics extractor tool (#38)
    * Add a Kaitai definition for the G2D binary file format which includes NCGR and NCLR files
    
    * Compile the G2D Kaitai definition into a Python parser
    
    * Add the 'extract_graphics' tool which can extract a greyscale image from an NCGR file
    
    * Extract the colour palette from the NCLR file and colour the NCGR image
    
    * Make the colour palette optional for the 'extract_graphics' tool
    
    * Modify the G2D Kaitai definition to correctly parse 8bpp NCGR image files
    
    * Add missing function doc comments
    
    * Modify the 'extract_graphics' tool to handle 8bpp NCGR image files
    
    * Add missing comments
    
    * Recompile the G2D Python parser

---
+++ definitions/g2d.ksy
@@ -0,0 +1,98 @@
+meta:
+  id: g2d
+  endian: le
+  encoding: utf-8
+
+seq:
+  - id: header
+    type: header
+  - id: blocks
+    type: block
+    repeat: expr
+    repeat-expr: header.block_count
+    
+types:
+  header:
+    -webide-representation: '{magic}'
+    seq:
+      - id: magic
+        type: str
+        size: 4
+      - id: byte_order
+        type: u2
+      - id: version
+        type: u2
+      - id: file_size
+        type: u4
+      - id: header_size
+        type: u2
+      - id: block_count
+        type: u2
+
+  block:
+    -webide-representation: '{magic}'
+    seq:
+      - id: magic
+        type: str
+        size: 4
+      - id: size
+        type: u4
+      - id: data
+        type:
+          switch-on: magic
+          cases:
+            '"TTLP"': pltt_block
+            '"RAHC"': char_block
+            
+  #####################
+  #### NCLR Blocks ####
+  #####################
+        
+  pltt_block:
+    seq:
+      - id: colour_format
+        type: u4
+      - id: extended_palette
+        type: u4
+      - id: palette_data_size
+        type: u4
+      - id: palette_data_offset
+        type: u4
+    
+    instances:
+      palette_data:
+        pos: _io.pos - 16 + palette_data_offset
+        size: palette_data_size
+        
+  #####################
+  #### NCGR Blocks ####
+  #####################
+  
+  char_block:
+    seq:
+      - id: width
+        type: u2
+      - id: height
+        type: u2
+      - id: colour_format
+        type: u4
+      - id: mapping_mode
+        type: u4
+      - id: graphics_type
+        type: u4
+      - id: graphics_data_size
+        type: u4
+      - id: graphics_data_offset
+        type: u4
+      - id: graphics_data
+        type: graphics_data(4 << colour_format)
+        size: graphics_data_size
+          
+  graphics_data:
+    params:
+      - id: tile_size
+        type: u4
+    seq:
+      - id: tiles
+        size: tile_size
+        repeat: eos
---
+++ python-package/src/ndsware/extract_graphics.py
@@ -0,0 +1,147 @@
+"""
+A tool for extracting images stored using the G2D binary format (i.e., NCGR and NCLR files).
+
+Author: CodeDragon82
+Data: 09/06/2025
+"""
+
+import click
+from ndsware.parsers.g2d import G2d
+from PIL import Image
+
+Pixel = tuple[int, int, int, int]
+
+TILE_WIDTH = 8
+TILE_HEIGHT = 8
+GREY_SCALE = [(i * 17, i * 17, i * 17, 255) for i in range(256)]
+
+
+def bgr555_to_rgb(colour: int) -> Pixel:
+    """Converts from BGR555 2-byte format to RGB tuple format."""
+
+    r = (colour & 0x1F) << 3
+    g = ((colour >> 5) & 0x1F) << 3
+    b = ((colour >> 10) & 0x1F) << 3
+
+    return (r, g, b, 255)
+
+
+def decode_palette(pltt_block: G2d.PlttBlock) -> list[Pixel]:
+    """Decodes the raw palette data into a list of colour pixels."""
+
+    palette_data = pltt_block.palette_data
+    palette = []
+
+    for i in range(0, len(palette_data), 2):
+        colour_bytes = int.from_bytes(palette_data[i : i + 2], "little")
+        colour = bgr555_to_rgb(colour_bytes)
+        palette.append(colour)
+
+    return palette
+
+
+def decode_4bpp_tile(tile: bytes, palette: list[Pixel]) -> list[Pixel]:
+    """
+    Decodes the raw tile data into a list of coloured pixels.
+
+    Each byte contains 2 pixels, one pixel per 4-bits. The 4-bits is integer
+    that represents an index in the colour palette.
+    """
+    pixels = []
+
+    for byte in tile:
+        low_nibble = byte & 0x0F
+        high_nibble = (byte >> 4) & 0x0F
+        pixels.append(palette[low_nibble])
+        pixels.append(palette[high_nibble])
+
+    return pixels
+
+
+def decode_8bpp_tile(tile: bytes, palette: list[Pixel]) -> list[Pixel]:
+    """
+    Decodes the raw tile data into a list of coloured pixels.
+
+    Each byte contains 1 pixel.
+    """
+
+    return [palette[byte] for byte in tile]
+
+
+def generate_tile_image(tile: bytes, palette: list[Pixel], colour_format: int) -> Image.Image:
+    """Generate a tile image object from the raw tile data."""
+
+    pixels = []
+    if colour_format == 0x3:
+        pixels = decode_4bpp_tile(tile, palette)
+    elif colour_format == 0x4:
+        pixels = decode_8bpp_tile(tile, palette)
+    else:
+        raise ValueError(f"Unknown colour format: {colour_format}")
+
+    tile_image = Image.new("RGBA", (TILE_WIDTH, TILE_HEIGHT))
+    tile_image.putdata(pixels)
+
+    return tile_image
+
+
+def get_block(g2d: G2d, block_type: str) -> G2d.CharBlock | G2d.PlttBlock:
+    """Get a block of a specific type from a parsed G2D file."""
+
+    block: G2d.Block
+    for block in g2d.blocks:
+        if block.magic == block_type[::-1]:
+            return block.data
+
+    raise KeyError(f"Missing {block_type} block.")
+
+
+@click.group()
+def cli() -> None:
+    """
+    Extracts images stored using the G2D binary format.
+    """
+
+
+@cli.command(help="Extract the image from an NCGR file.")
+@click.argument("ncgr_file", type=str)
+@click.option("-p", "--nclr-file", type=str, help="Colour palette (.nclr) file path.")
+@click.argument("tiles_per_row", type=int)
+@click.argument("output_image_file", type=click.Path(exists=False))
+def extract(ncgr_file: str, nclr_file: str, tiles_per_row: int, output_image_file: str) -> None:
+    """Extracts an image stored in a NCGR file."""
+
+    if not output_image_file.lower().endswith((".png", ".bmp")):
+        raise click.BadParameter("Output file must be a valid image type (PNG or BMP).")
+
+    if nclr_file:
+        nclr = G2d.from_file(nclr_file)
+        pltt_block: G2d.PlttBlock = get_block(nclr, "PLTT")
+        palette = decode_palette(pltt_block)
+    else:
+        palette = GREY_SCALE
+
+    ncgr = G2d.from_file(ncgr_file)
+    char_block: G2d.CharBlock = get_block(ncgr, "CHAR")
+
+    tiles = char_block.graphics_data.tiles
+    tile_count = len(tiles)
+    rows = (tile_count + tiles_per_row - 1) // tiles_per_row
+
+    image = Image.new("RGBA", (tiles_per_row * TILE_WIDTH, rows * TILE_HEIGHT))
+
+    for i, tile in enumerate(tiles):
+        tile_image = generate_tile_image(tile, palette, char_block.colour_format)
+
+        # Calculate tile position in image.
+        x = (i % tiles_per_row) * TILE_WIDTH
+        y = (i // tiles_per_row) * TILE_HEIGHT
+
+        # Paste tile into final image.
+        image.paste(tile_image, (x, y))
+
+    image.save(output_image_file)
+
+
+if __name__ == "__main__":
+    cli()
